25e0300a4bcd211c22ee4d125f40e5a8eb3ac35a
Daniil Sorokin <99132300+Deadpul378@users.noreply.github.com>
2022-08-12 20:20:40 +0300

    complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,15 @@ import Vimeo from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-const currentTime = 'current-time';
+const player = new Vimeo(iframe);
+const CURRENT_TIME = 'current-time';
 
 const onPlay = function (e) {
-  localStorage.setItem(currentTime, e.seconds);
+  localStorage.setItem(CURRENT_TIME, e.seconds);
 };
 player.on('timeupdate', throttle(onPlay, 1000));
 
-const savedTime = localStorage.getItem(currentTime);
+const savedTime = localStorage.getItem(CURRENT_TIME);
 if (savedTime) {
   player.setCurrentTime(savedTime);
 }
