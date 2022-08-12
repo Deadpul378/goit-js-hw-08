7af96829e3377987cef876589ee49e3f3c0ea49c
Daniil Sorokin <99132300+Deadpul378@users.noreply.github.com>
2022-08-12 18:37:58 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
-
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = createGalleryItem(galleryItems);
+function createGalleryItem(item) {
+  return item
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+    </a>`
+    )
+    .join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
+lightbox.options.captionDelay = '250';
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const currentTime = 'current-time';
+
+const onPlay = function (e) {
+  localStorage.setItem(currentTime, e.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem(currentTime);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
